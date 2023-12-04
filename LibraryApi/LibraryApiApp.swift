0caa4d7a8c7f54bdf467cc2d127dc05edf84f7d7
Javier Machuca <javier.machuca.a@gmail.com>
2023-12-04 20:52:49 -0300

    Initial Commit

---
+++ LibraryApi/LibraryApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApiApp.swift
+//  LibraryApi
+//
+//  Created by Javier Ignacio Machuca Arredondo on 04-12-23.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
